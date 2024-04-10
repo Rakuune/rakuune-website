194ece12f43d7ec4121303b0b566c24dd511220c
Rakuune <rakurantala@gmail.com>
2024-04-10 19:07:24 +0300

    some napit toimii ja taito container lisätty

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,15 +7,6 @@ import nameService from './services/Names'
 import './index.css'
 
 const App = () => {
-    /*
-        useEffect(() => {
-            nameService
-                .getAll()
-                .then(initialNames => {
-                    handler.setPersons(initialNames)
-                })
-        }, [])
-    */
     return (
         <div className="content">
             <div className="topnav">
@@ -28,44 +19,93 @@ const App = () => {
                 <h1>Testi nyt alkuun</h1>
             </div>
             <div className="profile">
-                <div className="profile-image">
-                    <img src="/profpic.png" alt="Valtteri Rantala"></img>
-                </div>
                 <div className="profile-content">
                     <h2>About me</h2>
                     <p>
-                    As a student at Oulu University, I've always believed in going beyond the traditional confines of classroom learning. 
-                    That's why I've taken it upon myself to dive deep into the world of full-stack programming, 
-                    learning independently and nurturing a growing passion for technology. 
-                    My interest isn't just academic; it's a reflection of my genuine enthusiasm for understanding 
-                    and mastering the digital tools that shape our world.
-                    People often describe me as down-to-earth, and I take that as a compliment. 
-                    It's this approachable nature that I believe helps me connect with both new concepts and people easily. 
-                    I pride myself on my adaptability and my ability to pick up new skills quickly. 
-                    Whether it's a complex programming language or a new project management tool, 
-                    I find excitement in the challenge of learning and applying knowledge swiftly and efficiently.
-                    My journey into coding and software development extends beyond just preparing for a future career;
-                     it's about cultivating a lifelong passion. I'm committed to staying ahead of the curve,
-                      eagerly embracing new technologies and methodologies that emerge in the ever-evolving landscape of full-stack development.
-                    To me, every piece of code is not just a task but a step towards building a more technologically adept future for myself and those around me.
-                    In summary, my academic journey at Oulu University, 
-                    combined with my self-taught programming endeavors, reflect not just my ambition but also my dedication to personal growth and professional excellence in the realm of technology.
+                        As a student at Oulu University, I've always believed in going beyond the traditional confines of classroom learning.
+                        That's why I've taken it upon myself to dive deep into the world of full-stack programming,
+                        learning independently and nurturing a growing passion for technology.
+                        My interest isn't just academic; it's a reflection of my genuine enthusiasm for understanding
+                        and mastering the digital tools that shape our world.
                     </p>
+
+                    <p>People often describe me as down-to-earth, and I take that as a compliment.
+                        It's this approachable nature that I believe helps me connect with both new concepts and people easily.
+                        I pride myself on my adaptability and my ability to pick up new skills quickly.
+                        Whether it's a complex programming language or a new project management tool,
+                        I find excitement in the challenge of learning and applying knowledge swiftly and efficiently.
+                    </p>
+
+                    <p>My journey into coding and software development extends beyond just preparing for a future career;
+                        it's about cultivating a lifelong passion. I'm committed to staying ahead of the curve,
+                        eagerly embracing new technologies and methodologies that emerge in the ever-evolving landscape of full-stack development.
+                        To me, every piece of code is not just a task but a step towards building a more technologically adept future for myself and those around me.
+                    </p>
+                    
+                    <p>In summary, my academic journey at Oulu University,
+                        combined with my self-taught programming endeavors, 
+                        reflect not just my ambition but also my dedication 
+                        to personal growth and professional excellence in the realm of technology.
+                    </p>
+                </div>
+                <div className="profile-image">
+                    <img src="/profpic.png" alt="Valtteri Rantala"></img>
+                </div>
+            </div>
+            <div className="skills">
+                <div className="skill-container">
+                    <h3>Skills</h3>
+                    <p>Some of the SW skills I've picked up in my studies or in my own time</p>
+                    <ul className="skills ul">
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Node.js</li>
+                        <li>Express</li>
+                        <li>MongoDB</li>
+                        <li>python</li>
+                        <li>Java</li>
+                    </ul>
                 </div>
             </div>
             <div>
-                <div className="header">
-                    <h2>Contact form</h2>
+                <div className="contactContainer">
+                    <ul className="container">
+                        <li className="item">
+                            <a href="https://www.instagram.com/valzhah/">
+                                <i className="fa-brands fa-instagram icon"></i>
+                            </a>
+                        </li>
+                        <li className="item">
+                            <a href="https://www.linkedin.com/in/valtteri-rantala-779027263/">
+                                <i className="fa-brands fa-linkedin icon"></i>
+                            </a>
+                        </li>
+                        <li className="item">
+                            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                                <i className="fa-brands fa-youtube icon"></i>
+                            </a>
+                        </li>
+                        <li className="item">
+                            <a href="https://github.com/Rakuune">
+                                <i className="fa-brands fa-github icon"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <ContactForm
+                        newName={handler.newName}
+                        newNumber={handler.newNumber}
+                        newEmail={handler.newEmail}
+                        message={handler.message}
+                        handleNameChange={handler.handleNameChange}
+                        handleNumberChange={handler.handleNumberChange}
+                        addName={handler.addName}
+                    />
                 </div>
-                <ContactForm
-                    newName={handler.newName}
-                    newNumber={handler.newNumber}
-                    newEmail={handler.newEmail}
-                    message={handler.message}
-                    handleNameChange={handler.handleNameChange}
-                    handleNumberChange={handler.handleNumberChange}
-                    addName={handler.addName}
-                />
+            </div>
+            <div className="footer">
+                Website by me
             </div>
         </div>
     )
--- part1/src/components/ContactForm.jsx
+++ part1/src/components/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './ContactForm.css';
+import '../index.css';
 const ContactForm = () => {
   const [formData, setFormData] = useState({
     name: '',
@@ -23,6 +24,7 @@ const ContactForm = () => {
 
   return (
     <form onSubmit={handleSubmit} className="contactForm">
+      <h2>Contact Form</h2>
       <div className="inputField">
         <input
           type="text"
@@ -30,7 +32,7 @@ const ContactForm = () => {
           value={formData.name}
           onChange={handleChange}
           placeholder="Name"
-          className="inputField" 
+          className="inputField"
         />
       </div>
       <div className="inputField">
@@ -40,7 +42,7 @@ const ContactForm = () => {
           value={formData.email}
           onChange={handleChange}
           placeholder="Email"
-          className="inputField" 
+          className="inputField"
         />
       </div>
       <div className="inputField">
@@ -50,7 +52,7 @@ const ContactForm = () => {
           value={formData.phone}
           onChange={handleChange}
           placeholder="Phone"
-          className="inputField" 
+          className="inputField"
         />
       </div>
       <div className="inputField">
@@ -59,7 +61,7 @@ const ContactForm = () => {
           value={formData.message}
           onChange={handleChange}
           placeholder="Message"
-          className="textAreaField" 
+          className="textAreaField"
         />
       </div>
       <button type="submit" className="Button">
